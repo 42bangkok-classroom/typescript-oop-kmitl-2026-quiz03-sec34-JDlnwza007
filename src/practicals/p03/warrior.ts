efb5608b64f449cd9ed0c9d5d3455184a697ab02
JDlnwza007 <68010147@kmitl.ac.th>
2026-02-20 18:15:54 +0700

    3

--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,15 @@
 import { Character } from "./character";
-
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+  private weapon: string;
+  constructor(name: string, weapon: string, health: number) {
+    super(name, health); 
+    this.weapon = weapon;
+  }
+  getWeapon(): string {
+    return this.weapon;
+  }
+  override receiveDamage(damage: number): void {
+    const actualDamage = damage * 0.9;
+    super.receiveDamage(actualDamage);
+  }
 }
