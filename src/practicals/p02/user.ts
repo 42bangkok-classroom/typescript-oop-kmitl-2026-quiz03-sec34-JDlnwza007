80dfec57e536d7aeba29debb81e0ecd5a6fa4dce
JDlnwza007 <68010147@kmitl.ac.th>
2026-02-20 18:15:18 +0700

    2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,26 @@
-export class User {}
+export class User {
+  private firstname: string = "";
+  private lastname: string = "";
+  private age: number = 0;
+  static BIRTH_YEAR: number = 2000;
+  
+  setFirstname(firstname: string) {
+    this.firstname = firstname;
+  }
+
+  setLastname(lastname: string) {
+    this.lastname = lastname;
+  }
+
+  setAge(age: number) {
+    this.age = age;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getFullName() {
+    return `${this.firstname} ${this.lastname}`;
+  }
+}
